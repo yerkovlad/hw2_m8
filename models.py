50cb590c29c31613b84ea8fc3e9bde0bc117c418
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-11-12 17:07:53 +0200

    Update models.py

--- models.py
+++ models.py
@@ -10,3 +10,10 @@ class Quote(Document):
     tags = ListField(StringField())
     author = ReferenceField(Author)
     quote = StringField()
+
+class Contact(Document):
+    fullname = StringField(required=True)
+    email = StringField(required=True)
+    sent_message = BooleanField(default=False)
+    phone_number = StringField()
+    preferred_contact_method = StringField(choices=["email", "sms"], default="email")
