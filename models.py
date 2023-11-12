79fae1071fda1c9b93b287b1c3a3935b4b57e626
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-11-12 17:05:18 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,13 @@
+# models.py
+from mongoengine import Document, StringField, DateTimeField, ReferenceField, ListField
+
+class Author(Document):
+    fullname = StringField(required=True)
+    born_date = DateTimeField()
+    born_location = StringField()
+    description = StringField()
+
+class Quote(Document):
+    tags = ListField(StringField())
+    author = ReferenceField(Author)
+    quote = StringField()
